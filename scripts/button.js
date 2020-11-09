1dac100997927af4f92d22cac387ff892c7b5d2a
nika <903889@pdsb.net>
2020-11-09 18:11:19 -0500

    :)

---
+++ scripts/button.js
@@ -0,0 +1,24 @@
+
+function whileSumThrees() {
+    let sum = 0;
+    let i = 0;
+    while (i < 1000) {
+        sum += i;
+        i = i + 3;
+    }
+    console.log(sum);
+
+}
+
+<script>
+function myFunction() {
+  var txt;
+  var person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    txt = "User cancelled the prompt."; 
+   } else {
+    txt = "hello" + person + "! how are you today?";
+
+  document.getElementById.innerHTML = txt;
+}
+</script>
