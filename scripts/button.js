b986f8325322c67ea5c8b5fe4d15c7a628e80965
nika <903889@pdsb.net>
2020-11-11 13:30:14 -0500

    *

--- scripts/button.js
+++ scripts/button.js
@@ -10,8 +10,3 @@ function whileSumThrees() {
 
 }
 
-// When the user clicks on <div>, open the popup
-function myFunction(myPopup) {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
