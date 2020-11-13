5abfb1a2a0c3106cce4bb1533c9a6fc70b934484
nika <903889@pdsb.net>
2020-11-13 08:29:39 -0500

    :)

---
+++ scripts/encryption.js
@@ -0,0 +1,36 @@
+const CryptoJS = require('crypto-js');
+
+const encrypt = text => {
+  return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(text));
+};
+
+const decrypt = data => {
+  return CryptoJS.enc.Base64.parse(data).toString(CryptoJS.enc.Utf8);
+};
+
+
+import AES from 'crypto-js/aes';
+import Utf8 from 'crypto-js/enc-utf8';
+
+const encryptWithAES = (text, passphrase) => {
+  return AES.encrypt(text, passphrase).toString();
+};
+
+const decryptWithAES = (ciphertext, passphrase) => {
+  const bytes = AES.decrypt(ciphertext, passphrase);
+  const originalText = bytes.toString(Utf8);
+  return originalText;
+};
+
+global.testAES = () => {
+  const inputText = 'Hello World';
+  const passphrase = 'My Secret Passphrase';
+
+  Logger.log({ inputText });
+
+  const encryptedText = encryptWithAES(inputText, passphrase);
+  Logger.log({ encryptedText });
+
+  const decryptedText = decryptWithAES(encryptedText, passphrase);
+  Logger.log({ decryptedText });
+};
